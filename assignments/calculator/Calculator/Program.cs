8546acf21014c1aa8f0b6f21fde13c783280b5ce
klaramikul <klmikulaskova@gmail.com>
2024-10-11 10:03:59 +0200

    oprava camelCase

--- assignments/calculator/Calculator/Program.cs
+++ assignments/calculator/Calculator/Program.cs
@@ -20,7 +20,7 @@ namespace Calculator
         /// Kontroluje spravny vstup operace
         /// </summary>
         /// <returns></returns>
-        static int getValidOperation()
+        static int GetValidOperation()
         {
             int x;
             while (true) //pokud je zjisteno nevhodne cislo, uzivatel bude opakovane zadan o zadani jineho
@@ -37,13 +37,13 @@ namespace Calculator
         /// Kontroluje spravny int vstup
         /// </summary>
         /// <returns></returns>
-        static int getValidInt()
+        static int GetValidInt()
         {
             Console.WriteLine("Zadej cele cislo.");
             int x;
             while (!int.TryParse(Console.ReadLine(), out x))
             {
-                invalidEntry();
+                InvalidEntry();
             }
             return x;
         }
@@ -52,13 +52,13 @@ namespace Calculator
         /// Kontroluje spravny float vstup
         /// </summary>
         /// <returns></returns>
-        static float getValidFloat ()
+        static float GetValidFloat ()
         {
             float x;
             Console.WriteLine("Zadej cislo.");
             while (!float.TryParse(Console.ReadLine(), out x))
             {
-                invalidEntry();
+                InvalidEntry();
             }
             return x;
         }
@@ -66,7 +66,7 @@ namespace Calculator
         /// <summary>
         /// Nevhodny vstup
         /// </summary>
-        static void invalidEntry()
+        static void InvalidEntry()
         {
             Console.WriteLine("Neplatny vstup. Zadej cislo.");
         }
@@ -74,7 +74,7 @@ namespace Calculator
         /// <summary>
         /// Upozorneni, ze nelze delit nulou
         /// </summary>
-        static void divideZero()
+        static void DivideZero()
         {
             Console.WriteLine("Nelze delit nulou.");
         }
@@ -86,7 +86,7 @@ namespace Calculator
         /// <param name="x">prvni scitanec</param>
         /// <param name="y">druhy scitanec</param>
         /// <returns> soucet (float) </returns>
-        static float add(float x, float y)
+        static float Add(float x, float y)
         {
             return x + y;
         }
@@ -97,7 +97,7 @@ namespace Calculator
         /// <param name="x">mensenec</param>
         /// <param name="y">mensitel</param>
         /// <returns> rozdil (float) </returns>
-        static float subtract(float x, float y)
+        static float Subtract(float x, float y)
         {
             return x - y;
         }
@@ -108,7 +108,7 @@ namespace Calculator
         /// <param name="x">prvni cinitel</param>
         /// <param name="y">druhy cinitel</param>
         /// <returns>soucin (float)</returns>
-        static float multiply(float x, float y)
+        static float Multiply(float x, float y)
         {
             return x * y;
         }
@@ -119,7 +119,7 @@ namespace Calculator
         /// <param name="x">delenec</param>
         /// <param name="y">delitel</param>
         /// <returns>podil (float)</returns>
-        static float divide(float x, float y)
+        static float Divide(float x, float y)
         {
             return x / y;
         }
@@ -129,7 +129,7 @@ namespace Calculator
         /// </summary>
         /// <param name="x">odmocnenec</param>
         /// <returns>odmocnina (float)</returns>
-        static float squareRoot(float x)
+        static float SquareRoot(float x)
         {
             return (float)Math.Sqrt(x);
         }
@@ -139,7 +139,7 @@ namespace Calculator
         /// </summary>
         /// <param name="x">mocnenec</param>
         /// <returns>mocnina (vstup)</returns>
-        static float squared(float x)
+        static float Squared(float x)
         {
             return (float)Math.Sqrt(x);
         }
@@ -150,7 +150,7 @@ namespace Calculator
         /// <param name="x">vychozi cislo</param>
         /// <param name="baseValue">zaklad soustavy</param>
         /// <returns>prevedene cislo (string)</returns>
-        static string convertToBase(int x, int baseValue)
+        static string ConvertToBase(int x, int baseValue)
         {
             return Convert.ToString(x, baseValue);
         }
@@ -166,72 +166,72 @@ namespace Calculator
 
             Console.WriteLine("Vyber si operaci a zadej: \n 1 pro soucet \n 2 pro rozdil \n 3 pro soucin \n 4 pro podil \n 5 pro odmocninu \n 6 pro mocninu \n 7 pro prevod do binarni soustavy \n 8 pro prevod do osmickove soustavy \n 9 pro prevod do sestnactkove soustavy  ");
             
-            operation = getValidOperation(); //zkontroluje, zda uzivatel zadal platnou operaci
+            operation = GetValidOperation(); //zkontroluje, zda uzivatel zadal platnou operaci
             
             if (operation <= 6) //vylouci z operaci prevody mezi ciselnymi soustavami
             {
-                number1 = getValidFloat(); //nacteni prvniho vhodneho vstupu
+                number1 = GetValidFloat(); //nacteni prvniho vhodneho vstupu
 
                 if (operation <= 4) //pta se na druhy vstup pouze pokud jsou k operaci potreba dva vstupy
                 {
-                    number2 = getValidFloat(); //nacteni druheho vhodneho vstupu
+                    number2 = GetValidFloat(); //nacteni druheho vhodneho vstupu
                 }
             }
 
             switch (operation)
             {
                 case 1:
-                    result = add(number1, number2); //soucet
+                    result = Add(number1, number2); //soucet
                     Console.WriteLine("Vysledek je " +  result);
                     break;
 
                 case 2:
-                    result = subtract(number1, number2); //rozdil
+                    result = Subtract(number1, number2); //rozdil
                     Console.WriteLine("Vysledek je " + result);
                     break;
 
                 case 3:
-                    result = multiply(number1, number2); //nasobeni
+                    result = Multiply(number1, number2); //nasobeni
                     Console.WriteLine("Vysledek je " + result);
                     break;
 
                 case 4:
                     if (number2 != 0)
                     {
-                        result = divide(number1, number2); //deleni
+                        result = Divide(number1, number2); //deleni
                         Console.WriteLine("Vysledek je " + result);
                     }
                     else //pri deleni nulou
                     {
-                        divideZero();
+                        DivideZero();
                     }
                     break;
 
                 case 5:
-                    result = squareRoot(number1); //odmocneni
+                    result = SquareRoot(number1); //odmocneni
                     Console.WriteLine("Vysledek je " + result);
                     break;
 
                 case 6:
-                    result = squared(number1); //umocneni
+                    result = Squared(number1); //umocneni
                     Console.WriteLine("Vysledek je " + result);
                     break;
 
                 case 7:
-                    int toConvertBinary = getValidInt(); //kontrola vstupu (cele cislo)
-                    string binaryResult = convertToBase(toConvertBinary, 2); //prevod do binarni soustavy, zdroj: chat gpt
+                    int toConvertBinary = GetValidInt(); //kontrola vstupu (cele cislo)
+                    string binaryResult = ConvertToBase(toConvertBinary, 2); //prevod do binarni soustavy, zdroj: chat gpt
                     Console.WriteLine("Toto cislo ma v binarni soustave hodnotu  " + binaryResult);
                     break;
 
                 case 8:
-                    int toConvertOctal = getValidInt(); //kontrola vstupu (cele cislo)
-                    string octalResult = convertToBase(toConvertOctal, 8); //prevod do osmickove soustavy
+                    int toConvertOctal = GetValidInt(); //kontrola vstupu (cele cislo)
+                    string octalResult = ConvertToBase(toConvertOctal, 8); //prevod do osmickove soustavy
                     Console.WriteLine("Toto cislo ma v osmickove soustave hodnotu " + octalResult);
                     break;
 
                 case 9:
-                    int toConvertHexadecimal = getValidInt(); //kontrola vstupu (cele cislo)
-                    string hexadecimalResult = convertToBase(toConvertHexadecimal, 16); //prevod do sestnactkove soustavy
+                    int toConvertHexadecimal = GetValidInt(); //kontrola vstupu (cele cislo)
+                    string hexadecimalResult = ConvertToBase(toConvertHexadecimal, 16); //prevod do sestnactkove soustavy
                     Console.WriteLine("Toto cislo ma v sestnactkove soustave hodnotu " + hexadecimalResult);
                     break;
             }
